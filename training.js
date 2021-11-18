9ad03b2e9f8248d54c413f3f42f6eb2e911179e7
Oomaryaser <88951305+Oomaryaser@users.noreply.github.com>
2021-11-17 20:00:10 -0800

    Add files via upload

---
+++ training.js
@@ -0,0 +1,18 @@
+
+var names = ["Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura",];
+var  charAt = ["John", "Jen", "Jason","Jim" ]
+
+
+for (let i = 0; i < names.length; i++) {
+    const element = "hello  "+ names[i];
+    console.log(element);
+}
+
+
+var  charAt = ["John", "Jen", "Jason","Jim" ]
+
+
+for (let i = 0; i < charAt.length; i++) {
+    const element = "goodbay  "+ charAt[i];
+    console.log(element);
+}
